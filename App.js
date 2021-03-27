295b0f6f64cc7a938f9941b326a795f43939cab1
MJO7 <megh07joshi@gmail.com>
2021-03-27 16:05:44 +0530

    Buzz

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import fb from "./screens/fb";
+import in1 from "./screens/in1";
+import { Icon } from "react-native-elements";
+import "react-native-vector-icons";
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+const switchN = createBottomTabNavigator(
+  {
+    fb: { screen: fb },
+    in1: { screen: in1 },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const routeName = navigation.state.routeName;
+        if (routeName == "fb") {
+          return <Icon name="home" type="entypo" color={tintColor} />;
+        } else if (routeName == "in1") {
+          return <Icon name="home" type="entypo" color={tintColor} />;
+        }
+      },
+      tabBarOptions: {
+        activeTintColor: "black",
+        inactiveTintColor: "grey",
+      },
+    }),
+  }
+);
+
+const AppContainer = createAppContainer(switchN);
---
+++ screens/in1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default class in1 extends React.Component {
+  render() {
+    return (
+      <View style={{ marginTop: 100, alignSelf: "center" }}>
+        <TouchableOpacity>
+          <Text>Instagram</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
